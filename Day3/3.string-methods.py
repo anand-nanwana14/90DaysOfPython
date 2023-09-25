c2fbc8752a1d24bf6eea2ef7462a96681aa58d62
Anand Nanwana <121349107+anand-nanwana14@users.noreply.github.com>
2023-09-25 15:39:55 +0530

    Add files via upload

---
+++ Day3/3.string-methods.py
@@ -0,0 +1,22 @@
+a = "!!! Anand !!! Anand !!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Anand","Good"))
+print(a.split("n"))
+b = "welcome to my  ChanneL\n"
+print(b.capitalize())
+print(b.center(50))
+print(b.count("e"))
+print(b.endswith("L"))
+print(b.endswith("to",4,10))
+print(b.isalpha())
+print(b.isalnum())
+print(b.isprintable())
+c = "       "
+print(c.isspace())
+print(b.swapcase())
+print(b.startswith("w"))
+print(b.title())
+
