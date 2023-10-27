f098f28214147a0cc4f342c6417e5b96bcdffaa1
anand-nanwana14 <anandnanwana84@gmail.com>
2023-10-27 16:45:41 +0530

    First Commit

---
+++ Day28/28.Import-Works.py
@@ -0,0 +1,14 @@
+# import math
+# results = math.sqrt(9)*math.pi
+# print(results)
+
+# from math import sqrt,pi
+
+# results = sqrt(64)*pi
+# print(results)
+
+# import pandas as pd
+# pd.__version__
+
+import hello
+print("I am Fine")
