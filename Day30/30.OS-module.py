c2e346897fceba9e3475780a01c6fdbf18dc8d2c
anand-nanwana14 <anandnanwana84@gmail.com>
2023-11-01 16:37:39 +0530

    First Commit

---
+++ Day30/30.OS-module.py
@@ -0,0 +1,17 @@
+import os
+if (not os.path.exists("Data")): 
+ os.mkdir("Data")
+
+for i in range(1,91):
+ os.mkdir(f"Data/Day{i}")
+
+# if (os.path.exists("Data")):
+#  for i in range(1,11):
+#   os.rmdir(f"Data/day{i}")
+#   if i == 5:
+#    break
+# print(os.getcwd())
+# folders = os.listdir("Data")
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"Data/{folder}"))
