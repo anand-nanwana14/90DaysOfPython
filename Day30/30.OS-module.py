ce2d8e1c3eeda10c74daee59beba5b622ae8bdbf
Anand Nanwana <121349107+anand-nanwana14@users.noreply.github.com>
2023-11-10 16:44:29 +0530

    Update 30.OS-module.py

--- Day30/30.OS-module.py
+++ Day30/30.OS-module.py
@@ -5,13 +5,13 @@ if (not os.path.exists("Data")):
 for i in range(1,90):
  os.mkdir(f"Data/Day{i}")
 
-if (os.path.exists("Data")):
- for i in range(1,11):
-  os.rmdir(f"Data/day{i}")
-  if i == 5:
-   break
-print(os.getcwd())
-folders = os.listdir("Data")
-for folder in folders:
-    print(folder)
-    print(os.listdir(f"Data/{folder}"))
+# if (os.path.exists("Data")):
+#  for i in range(1,11):
+#   os.rmdir(f"Data/day{i}")
+#   if i == 5:
+#    break
+# print(os.getcwd())
+# folders = os.listdir("Data")
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"Data/{folder}"))
